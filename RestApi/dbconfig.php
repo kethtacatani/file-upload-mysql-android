c02afc8f243ca13552b34666185b785da6fcb54b
Keth Dominic Tacatani <115775088+kethtacatani@users.noreply.github.com>
2024-03-28 00:43:50 +0800

    Update dbconfig.php

--- RestApi/dbconfig.php
+++ RestApi/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
-$DBhost = "localhost";
-$DBuser = "root";
-$DBpassword ="";
-$DBname="test";
+$DBhost = "localhost";//you know the drill :))
+$DBuser = "root";//you know the drill :))
+$DBpassword ="";//you know the drill :))
+$DBname="test"; //you know the drill :))
 
 $conn = mysqli_connect($DBhost, $DBuser, $DBpassword, $DBname); 
 
